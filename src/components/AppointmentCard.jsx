f1979fcac119716248f163ef2050458c4903925b
Manu Incicco <manuciriaco@gmail.com>
2023-08-30 20:53:46 -0300

    Cambio menor en appointmentcard

--- src/components/AppointmentCard.jsx
+++ src/components/AppointmentCard.jsx
@@ -4,8 +4,8 @@ import { appointments } from "../services/appointments";
 function AppointmentCard({ appointment }) {
   return (
     <div>
-      <h3>{appointment.petName}</h3>
       <p>Fecha: {new Date(appointment.date).toLocaleDateString()}</p>
+      <h3>{appointment.petName}</h3>
       <p>Animal: {appointment.animal}</p>
       <p>Motivo: {appointment.motive}</p>
       <p>Comentario: {appointment.comment}</p>
